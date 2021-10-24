d6347ffdb2f5f37d1c094bc34d2258c9a1388cd3
kangelina86@mail.ru <kangelina86@mail.ru>
2021-10-24 13:33:47 +0300

    - Removed the code status check from shorten_link() - Removed the code status check from count_clicks_total()  - Changed the prints in check_link()

--- bitlinks.py
+++ bitlinks.py
@@ -26,7 +26,6 @@ class Bitlinks:
             "long_url": f"{link}"
         }
         response = requests.post(url=self.url, json=payload, headers=headers)
-        response.raise_for_status()
         logging.warning(response.status_code)
         return response.json()["link"]
 
@@ -41,7 +40,6 @@ class Bitlinks:
             "unit": "day"
         }
         response = requests.get(url=url_link, params=payload, headers=headers)
-        response.raise_for_status()
         logging.warning(response.status_code)
         return response.json()
 
@@ -49,9 +47,10 @@ class Bitlinks:
         try:
             try:
                 link = input("Введите полный адрес ссылки: ")
-                print("Битлинк: ", self.shorten_link(link))
+                print("Вы ввели длинную ссылку! \n Bitlink: ", self.shorten_link(link))
             except:
-                print("Сумма кликов всего:", self.count_clicks_total(link)["total_clicks"], "\n")
+                print("Вы ввели Bitlink! \n")
+                print("Сумма кликов Bitlink:", self.count_clicks_total(link)["total_clicks"], "\n")
         except requests.exceptions.HTTPError as exc:
             logging.warning(exc)
             print(exc)
