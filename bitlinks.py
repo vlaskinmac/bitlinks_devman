ae2dde3a5cb43b09fa0252649a11174860e175f7
kangelina86@mail.ru <kangelina86@mail.ru>
2021-10-29 10:11:35 +0300

    Reformatted code

--- bitlinks.py
+++ bitlinks.py
@@ -10,10 +10,10 @@ def shorten_link(link, token):
     url = "https://api-ssl.bitly.com/v4/bitlinks"
     headers = {
         "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     payload = {
-        "long_url": link
+        "long_url": link,
     }
     response = requests.post(url=url, json=payload, headers=headers)
     response.raise_for_status()
@@ -27,10 +27,10 @@ def count_clicks_total(link, token):
     url_link = f"{url}/{parsed_link.netloc}{parsed_link.path}/clicks/summary"
     headers = {
         "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     payload = {
-        "unit": "day"
+        "unit": "day",
     }
     response = requests.get(url=url_link, params=payload, headers=headers)
     response.raise_for_status()
@@ -45,7 +45,7 @@ def is_bitlink(token, link):
     url_link = f"{url}/{parsed_link.netloc}{parsed_link.path}"
     headers = {
         "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
     }
     response = requests.get(url=url_link, headers=headers)
     if response.ok:
@@ -60,21 +60,31 @@ def main():
         level=logging.WARNING,
         filename="logs.log",
         filemode="w",
-        format="%(asctime)s - [%(levelname)s] - %(message)s"
+        format="%(asctime)s - [%(levelname)s] - %(message)s",
     )
     try:
         if not is_bitlink(token=token, link=link):
             print(
                 "\nВы ввели длинную ссылку!\nBitlink: ",
-                shorten_link(link=link, token=token)
-                  )
+                shorten_link(
+                    link=link,
+                    token=token,
+                )
+            )
         else:
             print("\nВы ввели Bitlink!\nСумма кликов Bitlink:",
-                  count_clicks_total(link=link, token=token)["total_clicks"], "\n")
+                  count_clicks_total(
+                      link=link,
+                      token=token,
+                  )["total_clicks"], "\n",
+                  )
     except KeyError as exc:
         logging.warning(exc)
         print("Ошибка в параметре: ", exc)
-    is_bitlink(token=token, link=link)
+    is_bitlink(
+        token=token,
+        link=link,
+    )
 
 
 if __name__ == "__main__":
